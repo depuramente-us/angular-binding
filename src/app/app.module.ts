94121669c9b0b3161aafe85030f69f9e2fedd64b
Alexander Garcia <depuramente@gmail.com>
2023-08-06 14:56:48 -0500

    courses component removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { CoursesComponent } from './courses/courses.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    CoursesComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule
